6fe34a098fa0d57348ca642142162f0b98617e3e
Goomcz <Goomcz@users.noreply.github.com>
2022-08-06 22:27:02 +0900

    Update codingtest3.js

--- codingtest3.js
+++ codingtest3.js
@@ -1,9 +1,18 @@
-function solution(id_list, report, k) {
-    const id_list = ['muzi','frodo','neo','apeach'];
-    const report = ['muzi frodo','frodo neo','neo apeach','apeach muzi','muzi neo', 'frodo muzi','frodo muzi'];
-    const k = 2;
+const id_list = ['muzi','frodo','neo','apeach'];
+const report = ['muzi frodo','frodo neo','neo apeach','apeach muzi','muzi neo', 'frodo muzi','frodo muzi'];
+const k = 2;
 
-const set = new Set(report);
-const newReport = [...set];
-console.log(newReport);
+function solution(id_list, report, k) {
+
+    
+    const newReport = Array.from(new Set(report));
+    
+
+return newReport;
 }
+
+console.log(newReport);
+
+
+
+
