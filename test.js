72e056e44ebe2780a92dc74486cb263922f66ce6
Goomcz <Goomcz@users.noreply.github.com>
2022-08-11 18:49:15 +0900

    condingtest4_completed

--- test.js
+++ test.js
@@ -1,13 +1,57 @@
 
-const lottos = [1,21,33,45,15,26];
-const num_win = [1,23,42,12,33,34];
+const lottos = [0,0,33,45,15,26];
+const win_nums = [1,2,3,4,5,6];
+let lottosSum = lottos[0]+lottos[1]+lottos[2]+lottos[3]+lottos[4]+lottos[5];
+let winCounts = 0;
 
-const num_winMap = new Map([['1',num_win[0]], ['2',num_win[1]] ,['3',num_win[2]],['4',num_win[3]],
-['5',num_win[4]], ['6',num_win[5]]]); 
-const lottosMap = new Map([['1',lottos[0]], ['2',lottos[1]] ,['3',lottos[2]],['4',lottos[3]],
-['5',lottos[4]], ['6',lottos[5]]]);
 
-console.log(lottosMap);
+    let zeroArray = lottos.filter(number => number < 1);
+    let zeroCounts = zeroArray.length;
 
-lottosMap.forEach((key,value)=> console.log(num_winMap.has('key')));
+    for(let i = 0; i<win_nums.length; i++){
+
+        if(lottos.includes(win_nums[i]) == true){
+            winCounts+=1;}
+        }
+
+        
+        if(winCounts <= 0){
+            let minRank = 6 - winCounts;
+            let maxRank = 6 - (winCounts + zeroCounts);
+        }else{
+            let minRank = 7 - winCounts;
+            let maxRank = 7 - (winCounts + zeroCounts);
+        }
+            
+            
+            
+            
+            let answer = [maxRank,minRank];
+            console.log(answer);
+
+// max => 맞춘 개수 + zero 갯수에 맞는 등수
+// min => 맞춘 개수에 맞는 등수
+
+
+// 6개 맞추고 zero 0개  불일치 0개 => 1등 1등
+// 5개 맞추고 zero 1개  불일치 0개 => 1등 2등
+// 5개 맞추고 zero 0개  불일치 1개 => 2등 2등 
+// 4 => 3
+// 3 => 4
+// 2 = > 5
+
+// 1개 맞추고 zero 5개 불일치 0개  => 1등 6등
+// 1개 맞추고 zero 4개 불일치 1개  => 2등 6등
+// 1개 맞추고 zero 3개 불일치 2개  => 3등 6등
+// 1개 맞추고 zero 2개 불일치 3개  => 4등 6등
+// 1개 맞추고 zero 1개 불일치 4개  => 5등 6등
+// 1개 맞추고 zero 0개 불일치 5개  => 6등 6등
+
+// 0개 맞추고 zero 6개 불일치 0개  => 1등 6등
+// 0개 맞추고 zero 5개 불일치 1개  => 2등 6등
+// 0개 맞추고 zero 4개 불일치 2개  => 3등 6등
+// 0개 맞추고 zero 3개 불일치 3개  => 4등 6등
+// 0개 맞추고 zero 2개 불일치 4개  => 5등 6등
+// 0개 맞추고 zero 1개 불일치 5개  => 6등 6등
+// 0개 맞추고 zero 0개 불일치 6개  => 6등 6등
 
